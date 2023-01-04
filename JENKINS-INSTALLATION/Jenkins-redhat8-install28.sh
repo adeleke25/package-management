c97e5db89f0b4e82103227c35579bb3ae8439a0b
adeleke25 <118616196+adeleke25@users.noreply.github.com>
2023-01-03 21:49:25 -0400

    Update Jenkins-redhat8-install28.sh

--- JENKINS-INSTALLATION/Jenkins-redhat8-install28.sh
+++ JENKINS-INSTALLATION/Jenkins-redhat8-install28.sh
@@ -22,5 +22,6 @@ sudo systemctl enable jenkins
 sudo systemctl start jenkins
 # You can check the status of the Jenkins service using the command:
 sudo systemctl status jenkins
-sudo su - ec2-user
 echo "echo of jenkins installation"
+sudo su - ec2-user
+
