348531ed9291cd8f24f84ab89a095b514a135253
adeleke25 <118616196+adeleke25@users.noreply.github.com>
2023-05-08 11:54:06 -0300

    Update jenkins-ubuntu-installation.sh

--- JENKINS-INSTALLATION/jenkins-ubuntu-installation.sh
+++ JENKINS-INSTALLATION/jenkins-ubuntu-installation.sh
@@ -5,14 +5,18 @@ sudo apt update   # Update the repositories
 sudo apt install openjdk-11-jdk
 java -version
 # Install Jenkins
-sudo wget -O /etc/yum.repos.d/jenkins.repo \
-    https://pkg.jenkins.io/redhat-stable/jenkins.repo
-sudo rpm --import https://pkg.jenkins.io/redhat-stable/jenkins.io.key
-sudo dnf upgrade
-# Add required dependencies for the jenkins package
-sudo dnf install chkconfig java-devel
-sudo dnf install jenkins
+curl -fsSL https://pkg.jenkins.io/debian/jenkins.io-2023.key | sudo tee \
+  /usr/share/keyrings/jenkins-keyring.asc > /dev/null
+echo deb [signed-by=/usr/share/keyrings/jenkins-keyring.asc] \
+  https://pkg.jenkins.io/debian binary/ | sudo tee \
+  /etc/apt/sources.list.d/jenkins.list > /dev/null
+sudo apt-get update
+sudo apt-get install jenkins
 # Start Jenkins
+# Add jenkins user to docker group
+sudo usermod -aG docker jenkins  ( This is to enable jenkins user to execute docker commands)
+sudo systemctl restart docker.service (To restart docker.service)
+sudo echo "jenkins
 sudo systemctl daemon-reload  # To Register the Jenkins service 
 sudo systemctl start jenkins
 systemctl status jenkins
