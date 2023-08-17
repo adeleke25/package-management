67643b981e93a2d72d9aefaa8f7436186af67b9b
adeleke25 <118616196+adeleke25@users.noreply.github.com>
2023-08-16 21:12:23 -0300

    Update jenkins-ubuntu-installation.sh

--- JENKINS-INSTALLATION/jenkins-ubuntu-installation.sh
+++ JENKINS-INSTALLATION/jenkins-ubuntu-installation.sh
@@ -1,25 +1,24 @@
 
-# Jenkins Installation And Setup In AWS EC2 ubuntu Instance.
-# Installation of Java
-sudo apt update   # Update the repositories
-sudo apt install openjdk-11-jdk
+
+  # Install Jenkins. However,Jenkins requires Java in order to run.
+    sudo apt update
+sudo apt install openjdk-17-jre
 java -version
-# Install Jenkins
-curl -fsSL https://pkg.jenkins.io/debian/jenkins.io-2023.key | sudo tee \
+   
+curl -fsSL https://pkg.jenkins.io/debian-stable/jenkins.io-2023.key | sudo tee \
   /usr/share/keyrings/jenkins-keyring.asc > /dev/null
 echo deb [signed-by=/usr/share/keyrings/jenkins-keyring.asc] \
-  https://pkg.jenkins.io/debian binary/ | sudo tee \
+  https://pkg.jenkins.io/debian-stable binary/ | sudo tee \
   /etc/apt/sources.list.d/jenkins.list > /dev/null
 sudo apt-get update
 sudo apt-get install jenkins
-# Start Jenkins
-# Add jenkins user to docker group
+
 sudo usermod -aG docker jenkins  ( This is to enable jenkins user to execute docker commands)
 sudo systemctl restart docker.service (To restart docker.service)
 sudo echo "jenkins ALL=(ALL) NOPASSWD:ALL" | sudo tee /etc/sudoers.d/jenkins
 
-# Check jenkins shell to see if it bin/bash. if it shows false, change it to bash.
-sudo vi /etc/passwd
 sudo systemctl daemon-reload  # To Register the Jenkins service 
 sudo systemctl start jenkins
 systemctl status jenkins
+
+sudo vi /etc/passwd
